e5a9503b97e9eda913922c70184c2aee9e625af0
yi-rou <57440062+yi-rou@users.noreply.github.com>
2019-11-13 17:20:47 +0800

    編輯事件

---
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        print("飛吧,小雞");
+    }
+
+    private void Update()
+    {
+        print("跳啊跳~");
+    }
+}
---
+++ UnityProject/Assets/Scripts/chicken.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+    [Header("跳躍高度"),Range(10,999)]
+	public int jump = 19;
+	[Header("走路速度"),Range(1.5f,99.9f)]
+	public float speed = 4.5f;
+	[Tooltip("是否取得食物")]
+	public bool food;
+
+}
